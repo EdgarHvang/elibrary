acecbb318ef9b77084b1de5d61223cc680825361
EdgarHvang <Edgarhvang@gmail.com>
2023-08-29 11:22:04 -0500

    first commit

---
+++ workspace/src/main/edu.mum.cs.cs425.demowebapps.elibrary/com/example/workspace/WorkspaceApplication.java
@@ -0,0 +1,14 @@
+package com.example.workspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class WorkspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkspaceApplication.class, args);
+    }
+
+}
